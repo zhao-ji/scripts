617ce79c11a77a4ee07a3582515cfb5db6f6e7c7
p0f <BytesWarning@gmail.com>
2015-08-08 22:35:58 +0800

    fix: add space in slash

--- redix.py
+++ redix.py
@@ -1,17 +1,17 @@
 dict = {}
 
-for i in range(0,10):
+for i in range(0, 10):
     dict[i] = str(i)
 
-for i in range(0,26):
+for i in range(0, 26):
     dict[i+10] = chr(i+65)
 
-def redix(a,n):
+def redix(a, n):
     S = ''
-    while a>=n:
+    while a >= n:
         S += dict[a%n]
         a = a/n
     S += dict[a%n]
     print S[::-1]
 
-redix(255,16)
+redix(255, 16)
