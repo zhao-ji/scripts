ac9b4d9ba66993a55bb2c69c7185f1daff06604b
p0f <BytesWarning@gmail.com>
2015-08-08 22:45:06 +0800

    fixup! fix: remove the useless backspace

--- redix.py
+++ redix.py
@@ -6,12 +6,13 @@ for i in range(0, 10):
 for i in range(0, 26):
     dict[i+10] = chr(i+65)
 
+
 def redix(a, n):
     S = ''
     while a >= n:
-        S += dict[a%n]
+        S += dict[a % n]
         a = a/n
-    S += dict[a%n]
+    S += dict[a % n]
     print S[::-1]
 
 redix(255, 16)
