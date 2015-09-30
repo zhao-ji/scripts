117ecb895a79a63c806ceb57dbf10747472ad7c3
yulin <byteswarning@gmail.com>
2015-09-30 10:45:16 +0800

    add: udp-ping.py fix: tcp-ping.py

--- udp_ping.py
+++ udp_ping.py
@@ -7,7 +7,7 @@ from time import time
 
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-from scapy.all import sr1, IP, TCP
+from scapy.all import sr1, IP, UDP, DNS, DNSQR
 
 
 def get_ip(host):
@@ -17,12 +17,25 @@ def get_ip(host):
         return gethostbyname(host)
 
 
-def timestamp():
-    return int(str(int(time() * 100000))[-8:])
+def timestamp(need=1):
+    if need == 1:
+        return int(bin(int(time()*10**6))[-32:], 2)
+    elif need == 2:
+        return int(bin(int(time()*10**6))[-16:], 2), \
+            int(bin(int(time()*10**6))[-32:-16], 2)
 
 
-def time_on_the_road(stamp):
-    return (timestamp() - stamp + 1) / 100
+def time_on_the_road(stamp_base, stamp_high=0):
+    # if use udp ping, stamp split to two parts, each parts is 16 bit
+    # and if use tcp ping, stamp is 32 bits
+    stamp = stamp_high and stamp_high * 2 ** 16 + stamp_base or stamp_base
+
+    stamp_now = timestamp()
+    if stamp_now < stamp:
+        # the 32 bit stamp is overflow
+        stamp_now = stamp_now + 2 ** 32
+
+    return float(stamp_now - stamp) / (10 ** 3)
 
 
 def standard_deviation(li):
@@ -62,7 +75,7 @@ if __name__ == "__main__":
     parser.add_argument("host", help="the host you want to ping")
     parser.add_argument(
         "-p", "--port",
-        type=int, default=80,
+        type=int, default=53,
         help='the port your want to ping'
     )
     parser.add_argument(
@@ -78,7 +91,7 @@ if __name__ == "__main__":
         print "please check your domain name."
         raise SystemExit
     else:
-        print "PING {host} {host_ip} by TCP".format(
+        print "PING {host} {host_ip} by UDP".format(
             host=args.host,
             host_ip=host_ip,
         )
@@ -87,27 +100,41 @@ if __name__ == "__main__":
     actually_ping_count = 0
     start_time = time()
 
-    ans = sr1(IP(dst=host_ip)/TCP(dport=args.port, flags="S", seq=timestamp()),
-              verbose=0)
+    # ans = sr1(IP(dst=host_ip)/TCP(
+    #     dport=args.port, flags="S", seq=timestamp()),
+    #           verbose=0)
     for i in xrange(args.count):
+        stamp_base, stamp_high = timestamp(need=2)
+        print "send", stamp_base, stamp_high
+        print "type", type(stamp_base), type(stamp_high)
         ans = sr1(
-            IP(dst=host_ip)/TCP(
+            IP(
+                dst=host_ip
+            )/UDP(
+                sport=stamp_base,
                 dport=args.port,
-                flags="S",
-                seq=timestamp()),
+            )/DNS(
+                rd=0,
+                id=stamp_high,
+                qd=DNSQR(qname="www.baidu.com"),
+            ),
+            timeout=2,
             verbose=0,
         )
+        print 'recv', ans.dport, ans.id
+        print 'type', type(ans.dport), type(ans.id)
+        print 'now', timestamp(need=2)
         print ("from {host_ip}: "
-               "tcp_req={tcp_req} "
+               "udp_req={udp_req} "
                "ttl={ttl} "
                "time={time_on_road} ms"
                ).format(
                    host_ip=host_ip,
-                   tcp_req=i,
+                   udp_req=i,
                    ttl=ans.ttl,
-                   time_on_road=time_on_the_road(ans.ack),
+                   time_on_road=time_on_the_road(ans.dport, ans.id),
         )
-        rtt_list.append(time_on_the_road(ans.ack))
+        rtt_list.append(time_on_the_road(ans.dport, ans.id))
 
     print "\n--- {host} ping statistics ---".format(host=args.host)
     print ("{packets_total} packets transmitted, "
@@ -122,5 +149,5 @@ if __name__ == "__main__":
         min=min(rtt_list),
         avg=sum(rtt_list)/float(len(rtt_list)),
         max=max(rtt_list),
-        mdev=int(standard_deviation(rtt_list)),
+        mdev=standard_deviation(rtt_list),
     )
